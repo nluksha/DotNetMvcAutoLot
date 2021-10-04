184e967922a66de3275dbe698aeacb93816d2ffe
Nikita Luksha <nikita.luksha@softensity.com>
2021-10-04 22:30:34 +0300

    Generate Inventory controller

---
+++ DotNetMvcAutoLot/DotNetMvcAutoLot/Controllers/InventoryController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DotNetEFAutoLot.DAL.EF;
+using DotNetEFAutoLot.DAL.Models;
+using DotNetEFAutoLot.DAL.Repositories;
+
+namespace DotNetMvcAutoLot.Controllers
+{
+    public class InventoryController : Controller
+    {
+        private InventoryRepository repo = new InventoryRepository();
+
+        // GET: Inventory
+        public ActionResult Index()
+        {
+            return View(repo.GetAll());
+        }
+
+        // GET: Inventory/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inventory inventory = repo.GetOne(id);
+            if (inventory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventory);
+        }
+
+        // GET: Inventory/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Inventory/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Make,Color,PetName")] Inventory inventory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(inventory);
+            }
+
+            try
+            {
+                repo.Add(inventory);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(String.Empty, $@"Unable to create record: {ex.Message}");
+
+                return View(inventory);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: Inventory/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inventory inventory = db.Inventory.Find(id);
+            if (inventory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventory);
+        }
+
+        // POST: Inventory/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Make,Color,PetName")] Inventory inventory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(inventory);
+            }
+
+            try
+            {
+                repo.Add(inventory);
+            } catch (Exception ex)
+            {
+                ModelState.AddModelError(String.Empty, $@"Unable to create record: {ex.Message}");
+
+                return View(inventory);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: Inventory/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inventory inventory = db.Inventory.Find(id);
+            if (inventory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventory);
+        }
+
+        // POST: Inventory/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Inventory inventory = db.Inventory.Find(id);
+            db.Inventory.Remove(inventory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                repo.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
