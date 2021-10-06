9981a09a3fed2990d80894659aaf4f5583d33c1d
Nikita Luksha <nikita.luksha@softensity.com>
2021-10-06 20:03:52 +0300

    Add Inventory template

--- DotNetMvcAutoLot/DotNetEFAutoLot.DAL/PartialEntities/InventoryPartial.cs
+++ DotNetMvcAutoLot/DotNetEFAutoLot.DAL/PartialEntities/InventoryPartial.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using DotNetEFAutoLot.DAL.Models.MetaData;
 
 namespace DotNetEFAutoLot.DAL.Models
 {
+    [MetadataType(typeof (InventoryMetaData))]
     public partial class Inventory
     {
         [NotMapped]
