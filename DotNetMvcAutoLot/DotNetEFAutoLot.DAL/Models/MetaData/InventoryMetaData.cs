d975ec7e45fed5971115c6a161733546da9f310a
Nikita Luksha <nikita.luksha@softensity.com>
2021-10-06 20:52:26 +0300

    Update validation

--- DotNetMvcAutoLot/DotNetEFAutoLot.DAL/Models/MetaData/InventoryMetaData.cs
+++ DotNetMvcAutoLot/DotNetEFAutoLot.DAL/Models/MetaData/InventoryMetaData.cs
@@ -11,5 +11,8 @@ namespace DotNetEFAutoLot.DAL.Models.MetaData
     {
         [Display(Name ="Pet Name")]
         public string PetName;
+
+        [StringLength(50, ErrorMessage = "Please enter a value less than 50 characters long.")]
+        public string Make;
     }
 }
